4cb8e5ada149854d9fb7cb4c8456506ec647265f
m-daniela <54980615+m-daniela@users.noreply.github.com>
2020-02-26 15:12:31 +0200

    Added more on Book

---
+++ src/main/java/repository/BookFileRepository.java
@@ -0,0 +1,73 @@
+package repository;
+
+import domain.Book;
+import domain.Client;
+import domain.validators.Validator;
+import domain.validators.ValidatorException;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class BookFileRepository extends InMemoryRepository<Long, domain.Book> {
+    private String fileName;
+
+    public BookFileRepository(Validator<Book> validator) {
+        super(validator);
+    }
+
+    private void loadData() {
+        Path path = Paths.get(fileName);
+
+        try {
+            Files.lines(path).forEach(line -> {
+                List<String> items = Arrays.asList(line.split(","));
+
+                Long id = Long.valueOf(items.get(0));
+                String serialNumber = items.get(1);
+                String name = items.get((2));
+                String author = items.get((3));
+                int year = Integer.parseInt(items.get((4)));
+
+                domain.Book book = new domain.Book(serialNumber, name, author, year);
+                book.setId(id);
+
+                try {
+                    super.save(book);
+                } catch (ValidatorException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public Optional<Book> save(domain.Book entity) throws ValidatorException {
+        Optional<domain.Book> optional = super.save(entity);
+        if (optional.isPresent()) {
+            return optional;
+        }
+        saveToFile(entity);
+        return Optional.empty();
+    }
+
+    private void saveToFile(domain.Book entity) {
+        Path path = Paths.get(fileName);
+
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path, StandardOpenOption.APPEND)) {
+            bufferedWriter.write(
+                    entity.getId() + "," + entity.getSerialNumber() + "," + entity.getName());
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
