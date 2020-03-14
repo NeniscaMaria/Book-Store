d611a619f9aab40b3dd7497f2ac9038a4e2aa167
m-daniela <54980615+m-daniela@users.noreply.github.com>
2020-03-14 16:49:53 +0200

    Added Purchase class

---
+++ src/main/java/domain/Purchase.java
@@ -0,0 +1,72 @@
+package domain;
+
+import jdk.internal.net.http.common.Pair;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class Purchase extends BaseEntity<Long>{
+    // Purchase receives a Client, Book and a number of books
+    // the client wishes to purchase
+
+    Client client;
+    Book book;
+    int nrBooks;
+
+    public Purchase() {
+    }
+
+    public Purchase(Client client, Book book, int nrBooks) {
+        this.client = client;
+        this.book = book;
+        this.nrBooks = nrBooks;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public int getNrBooks() {
+        return nrBooks;
+    }
+
+    public void setNrBooks(int nrBooks) {
+        this.nrBooks = nrBooks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Purchase purchase = (Purchase) o;
+        return nrBooks == purchase.nrBooks &&
+                Objects.equals(client, purchase.client) &&
+                Objects.equals(book, purchase.book);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(client, book, nrBooks);
+    }
+
+    @Override
+    public String toString() {
+        return "Purchase{" +
+                "client=" + client +
+                ", book=" + book +
+                ", nrBooks=" + nrBooks +
+                '}' + super.toString();
+    }
+}
